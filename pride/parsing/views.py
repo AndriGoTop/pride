561e735bfafbd0972e4406510103af69edc50ed4
razumovskiss <nomerchik@gmail.com>
2024-10-26 01:31:10 +0300

    cs

--- pride/parsing/views.py
+++ pride/parsing/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import requests
 
-# Create your views here.
+def index(request):
+    text = {
+        'welcome_message': 'чета там Space'
+    }
+
+    return JsonResponse({
+        'message': text,
+        'tools_link': '/tool/',
+    })
+
+
+
+def tool(request):
+    text = {
+        'tool_description': 'хуйня с тулами '
+    }
+
+    return JsonResponse({
+        'tools': TOOLS
+    })
