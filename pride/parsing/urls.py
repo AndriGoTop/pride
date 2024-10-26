7e66ca615d3265e64c0f1b97e5ff1a5d8b223a1c
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2024-10-26 10:16:56 +0300

    rabota

--- pride/parsing/urls.py
+++ pride/parsing/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("products/", views.parsing_site),
-    path('qwerty/', views.news_view)
+    path('qwerty', views.news_view)
 ]
