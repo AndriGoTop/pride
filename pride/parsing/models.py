0ec52e4803491aa442142033ad62e0293889c3ca
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2024-11-06 23:27:36 +0300

    changes

--- pride/parsing/models.py
+++ pride/parsing/models.py
@@ -3,17 +3,12 @@ from modulefinder import Module
 from django.utils import timezone
 from django.db import models
 
-class Tools(models.Model):
-    image = models.ImageField(upload_to='intro_pictures', verbose_name='Обложка') # будет подгружать картинку инструмента
-    name = models.CharField(max_length=30, verbose_name="Название") # название инструмента
-    price = models.CharField(max_length=10, verbose_name="Цена") # название инструмента
-    # price_ozon = models.IntegerField(verbose_name="Цена ozon") # цена озон
-    # price_wb = models.IntegerField(verbose_name="Цена вб") # цена вб
-    url = models.CharField(max_length=255, verbose_name="url", unique=True) # url на яндекс маркет
-    # url_ozon = models.URLField(verbose_name="url ozon")  # url на ozon
-    # url_wb = models.URLField(verbose_name="url wb")  # url на wb)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')  # Установка значения по умолчанию
-    # еще нужно откуда то взять
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=100)
+    sku = models.CharField(max_length=50, unique=True)  # Уникальный идентификатор товара
+    last_checked = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
