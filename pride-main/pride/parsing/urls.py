5a2070153d6525ee8dedaf7867186bb7f8015ebe
Dmitriy <141348723+Dim4ik-creator@users.noreply.github.com>
2025-01-22 01:27:35 +0300

    Delete pride-main directory

--- pride-main/pride/parsing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path("", views.index, name='home'),
-    path("tool/<int:tool_id>", views.tools, name='tools'),
-    path('', views.articles_list, name='home'),
-    path("run_server", views.run_parser),
-]
